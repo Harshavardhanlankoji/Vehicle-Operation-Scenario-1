087793a09d0801519b3896045c1422dfe94c0624
Harshavardhanlankoji <144333570+Harshavardhanlankoji@users.noreply.github.com>
2024-05-19 18:08:03 +0530

    Add files via upload

---
+++ Vehicle-Scenario-main/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import Home from "./component/Home/Home";
+import Sidebar from "./component/Layout/Sidebar";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import AddScenario from "./component/Scenario/AddScenario";
+import AllScenario from "./component/Scenario/AllScenario";
+import AddVehicle from "./component/Home/AddVehicle";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/Add_scenario" replace />} />
+          <Route path="Home" element={<Home />}></Route>
+          <Route path="Add_scenario" element={<AddScenario />}></Route>
+          <Route path="updateScenario" element={<AddScenario />}></Route>
+          <Route path="All_scenario" element={<AllScenario />}></Route>
+          <Route path="Add_vehicle" element={<AddVehicle />}></Route>
+          <Route path="updateVehicles" element={<AddVehicle />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
